721bbacb5d3acc3a42704d987a7e7ad3a87f6f2a
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-16 21:39:10 -0300

    Adicionando nova modificação

--- fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/operadores.js
+++ fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/operadores.js
@@ -28,3 +28,13 @@ if(weekDay==="segunda-feira" || weekDay==="terça-feira"){
 }else{
     console.log("FINALMENTE, descanso merecido UwU");
 }
+
+
+const comida = 'pão na chapa';
+const bebida = 'cafezinho';
+
+if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
