a712e5b763de2b29feec18629ec810f982d155f3
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-20 15:40:24 -0300

    Exercicios seletores

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,7 @@
+document.getElementById("page-title").innerText = "Vingadores";
+document.getElementsByClassName("paia")[0].innerText = "Vou mudar mah"
+let x = document.getElementsByClassName("paia");
+x[0].style.color = 'red';
+
+let s = document.getElementsByTagName("h4");
+s[0].style.color = 'green';
