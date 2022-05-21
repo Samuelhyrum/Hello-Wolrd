90cc8fa21f34583b33b70513de8beb7714e63525
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-21 17:50:34 -0300

    Tasks 1 e 2

---
+++ fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,16 @@
+// arquivo script.js
+//Task 1
+document.querySelector("#lementoOndeVoceEsta");
+
+// Task 2
+let x = document.querySelector('#pai');
+x.style.backgroundColor = 'red';
+
+
+
+
+
+
+console.log(document.getElementById('start').nextSibling) // nó
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
