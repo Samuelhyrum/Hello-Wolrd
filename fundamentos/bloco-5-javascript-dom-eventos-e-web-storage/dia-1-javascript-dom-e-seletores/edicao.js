d79de648cfe8b8bddbe4f22f61c56574b6e90b53
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-21 09:52:57 -0300

    Segunda e Terceira tasks feitas

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/edicao.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/edicao.js
@@ -1,1 +1,15 @@
+//Primeira task
 document.getElementsByClassName("paragrafo")[1].innerText= " Me vejo daqui a 2 anos tentando ser melhor a cada código, a cada aprendizado";
+
+//Segunda task
+let main = document.getElementsByClassName("main-content");
+main[0].style.backgroundColor = "yellowgreen"
+
+// Teicera task
+let center = document.getElementsByClassName("center-content")
+center[0].style.backgroundColor = "white";
+
+//Quarta task
+
+
+
