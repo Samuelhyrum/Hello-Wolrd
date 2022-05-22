e7d0cb884f566921a819f25f8364016d9686cb05
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-22 15:18:46 -0300

    Colocando mais tasks

--- fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -24,6 +24,16 @@ document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
 //Task 8
 document.querySelector('#pai').lastElementChild.previousElementSibling;
 
+//Task 9
+let adotado = document.querySelector('#pai');
+let postiço = document.createElement('section');
+adotado.appendChild(postiço);
+
+//Task 10 
+let filho = document.querySelector("#elementoOndeVoceEsta");
+let MaisVelho = document.createElement('section');
+filho.appendChild(MaisVelho);
+
 
 
 
