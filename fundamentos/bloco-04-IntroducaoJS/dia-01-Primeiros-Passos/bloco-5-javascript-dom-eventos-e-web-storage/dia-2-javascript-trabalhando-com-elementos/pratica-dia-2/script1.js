53b3a757793439a409de93557cf9addbacce9f14
Samuel Hyrum <samuelhymp@gmail.com>
2022-05-23 15:38:48 -0300

    PRATICAAAgit add pratica-dia-2/

---
+++ fundamentos/bloco-04-IntroducaoJS/dia-01-Primeiros-Passos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/pratica-dia-2/script1.js
@@ -0,0 +1,8 @@
+//Task 1 
+let body = document.querySelector('body');
+let h1 = document.createElement('h1');
+body.appendChild(h1);
+h1.innerText = "Exercício 5.2 - JavaScript DOM";
+
+//Task 2 
+let main = document.createElement('main');
