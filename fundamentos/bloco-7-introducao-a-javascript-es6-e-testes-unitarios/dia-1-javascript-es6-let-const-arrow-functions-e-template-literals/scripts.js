b6af86f9aac3c6cbf346073a8da155adcae3fc96
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-08 20:55:34 -0300

    Ordenando numeros por ordem crescente

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripts.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripts.js
@@ -1,26 +1,34 @@
 function testingScope(escopo) {
     if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+        console.log(ifScope);
     } else {
-      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
     }
     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+}
 
-  testingScope(true);
+testingScope(true);
 
-  const testingScope = (escopo) =>{
-      if (escopo === true){
-          const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          console.log(`${ifScope}, ótimo, fui utilizada no escopo !`);
-      } else {
-          const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-          console.log(elseScope);
-      }
-  }
+const testingScope = (escopo) => {
+    if (escopo === true) {
+        const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        console.log(`${ifScope}, ótimo, fui utilizada no escopo !`);
+    } else {
+        const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
 
-  testingScope(false);
+testingScope(false);
 
+
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a,b) => a - b );
+
+console.log(oddsAndEvens); 
