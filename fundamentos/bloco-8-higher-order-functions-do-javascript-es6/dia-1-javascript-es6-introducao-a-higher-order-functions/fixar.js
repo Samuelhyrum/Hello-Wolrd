7b98f5c7ba13b2ebf303cd4ba090d6c0fe97cdf8
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-23 15:55:53 -0300

    Ajeitando com underline

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixar.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixar.js
@@ -22,9 +22,9 @@ const cadastro = (nome, mail) => ({
 
 const newEmployees = () => {
     const employees = {
-        id1: cadastro("Pedro Guerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-        id2: cadastro("Luiza Drummond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-        id3: cadastro("Carla Paiva") // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+        id1: cadastro("pedro_guerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: cadastro("luiza_drummond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: cadastro("carla_paiva") // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     };
     return employees;
 };
