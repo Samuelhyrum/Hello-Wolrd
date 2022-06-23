bc9bfc96a7d316fded39c332100c6d5db650bf4c
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-23 15:41:47 -0300

    Agora na pratica primeiro item

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixar.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixar.js
@@ -13,3 +13,20 @@ const doingThings = (Thing) => Thing();
 console.log(doingThings(sleeping));
 // console.log(wakeUp());
 // console.log(drinks());
+
+const cadastro = (nome, mail) => ({
+    name: nome,
+    mail: `${nome}@trybe.com`
+});
+//console.log(cadastro("samuel", "samuelhymp"));
+
+const newEmployees = () => {
+    const employees = {
+        id1: cadastro("Pedro Guerra"), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: cadastro("Luiza Drummond"), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: cadastro("Carla Paiva") // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    };
+    return employees;
+};
+
+console.log(newEmployees());
