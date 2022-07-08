feda994aeac2240f915ddded067c9810055635b8
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-08 14:53:04 -0300

    Exercicios 1 ao 4

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
@@ -1,30 +1,63 @@
-const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
-  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+//EXERCICIOS DO 1 A 2
 
-const mars = {
-  name: "Mars",
-  distanceFromSun: {
-    value: 227900000,
-    measurementUnit: "kilometers",
-  },
-};
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
-const venus = {
-  name: "Venus",
-  distanceFromSun: {
-    value: 108200000,
-    measurementUnit: "kilometers",
-  },
-};
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-const jupiter = {
-  name: "Jupiter",
-  distanceFromSun: {
-    value: 778500000,
-    measurementUnit: "kilometers",
-  },
-};
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
 
-console.log(planetDistanceFromSun(mars)); // A
-console.log(planetDistanceFromSun(venus)); // B
-console.log(planetDistanceFromSun(jupiter)); // C
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// console.log(planetDistanceFromSun(venus)); // B
+// console.log(planetDistanceFromSun(jupiter)); // C
+
+
+//EXERCICIO 3
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   console.log("Returned planet: ", mars);
+// };
+
+// setTimeout(()=> getPlanet(), 4000); // imprime Marte depois de 4 segundos
+
+//EXERCICIO 4
+
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// // crie a função sendMarsTemperature abaixo
+// const sendMarsTemperature = (temp) => {
+//     console.log(`Mars temperature is: ${temp} degree Celsius`);
+// };
+
+// setTimeout(() => sendMarsTemperature(getMarsTemperature()), messageDelay()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
