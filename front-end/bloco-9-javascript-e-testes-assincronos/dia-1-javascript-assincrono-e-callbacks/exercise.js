8bbb46c20313d7611b28d925159079c6ac2f86f5
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-08 22:18:07 -0300

    Fazendo metade do exercicio 8

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise.js
@@ -179,17 +179,19 @@ function getPokemonDetails(filter, callback) {
         if (pokemons.find(filter) === undefined) {
             return callback(new Error('Não temos esse pokémon para você :('), null);
         }
-        const pokemon = pokemons.find(filter => filter.name === filter2);
+        const pokemon = pokemons.find(filter);
 
         const { name, type, ability } = pokemon;
 
-        const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+       const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
 
-        callback(null, messageFromProfOak);
+       callback(null, messageFromProfOak);
     }, 2000);
 }
 
-getPokemonDetails('Squirtle', (callback) => {
+const param = parametro => parametro.name === 'Squirtle';
+
+getPokemonDetails(param, (callback) => {
     console.log(callback);
 });
 
