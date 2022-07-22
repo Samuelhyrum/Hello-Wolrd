0734d99d047b334f88d7d4e0b7bccfc9a0b4ab06
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-22 15:25:37 -0300

    Exercicios do course mudando a cor do botão se for par

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -5,47 +5,60 @@ class App extends React.Component {
   constructor() {
     super()
 
-    this.handleClick = this.handleClick.bind(this)
-    this.Click = this.Click.bind(this)
-    this.handle = this.handle.bind(this)
-
     this.state = {
       numeroCliques: 0,
       numero: 0,
-      num : 0
-    }
+      num: 0
+    };
 
+    this.handleClick = this.handleClick.bind(this)
+    this.Click = this.Click.bind(this)
+    this.handle = this.handle.bind(this)
   }
 
-  // par () {
-  //   this.setState((antigo, _props) => {if (antigo %2 ===0){
-  //    num : antigo.num +green
-  //   }
-  // })
-  // }
   handleClick() {
-    this.setState((antigo, _props) => ({
-      numero: antigo.numero + 1
-    }))
+    this.setState(({ numero }) => ({
+      numero: numero + 1
+    }), () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.numero)}`);
+    });
   }
+
   Click() {
-    this.setState((antigo, _props) => ({
-      num: antigo.num + 1
-    }))
+    this.setState(({ num }) => ({
+      num: num + 1
+    }), () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.num)}`);
+    });
   }
+
   handle() {
-    this.setState((antigo, _props) => ({
-      numeroCliques: antigo.numeroCliques + 1
-    }))
+    this.setState(({ numeroCliques }) => ({
+      numeroCliques: numeroCliques + 1
+    }), () => {
+      console.log(`Botão 2 ${this.getButtonColor(this.state.numeroCliques)}`);
+    });
+  }
+
+  getButtonColor(num) {
+    // Essa função contém um ternário que realiza a lógica para mudarmos
+    // a cor do botão para verde quando for um número par
+    return num % 2 === 0 ? 'green' : 'white';
   }
 
 
   render() {
+    const { numero, num, numeroCliques } = this.state;
+
     return (
       <div>
-        <button onClick={this.handleClick}>{this.state.numero}</button>
-        <button onClick={this.Click}>{this.state.num}</button>
-        <button onClick={this.handle}>{this.state.numeroCliques}</button>
+        <button onClick={this.handleClick}
+          style={{ backgroundColor: this.getButtonColor(numero) }}> Botão 1| Count = {numero}</button>
+
+        <button onClick={this.Click}
+          style={{ backgroundColor: this.getButtonColor(num) }}> Botão 2 | Count = {num}</button>
+
+        <button onClick={this.handle} style={{ backgroundColor: this.getButtonColor(numeroCliques) }}>Botão 3 | Count = {numeroCliques}</button>
 
       </div>
     )
