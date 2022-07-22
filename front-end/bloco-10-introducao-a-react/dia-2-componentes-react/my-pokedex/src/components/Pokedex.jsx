653f7ae044cff6ee0b4422eb24665325eb3f4a44
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-22 11:43:27 -0300

    Botão de next

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.jsx
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.jsx
@@ -1,18 +1,42 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import pokemons from '../data'
 import Pokemon from "./Pokemon";
 
 class Pokedex extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.nextPoke = this.nextPoke.bind(this);
+  }
+
+  nextPoke() {
+    const { count } = this.state;
+
+    if (count === pokemons.length - 1) {
+      this.setState(() => ({
+        count: 0,
+      }));
+    } else {
+      this.setState((antigo) => ({
+        count: antigo.count + 1,
+      }));
+    }
+  }
+
   render() {
+    const { count } = this.state;
+    return (
 
-    const pokemonHTMLElements = pokemons.map((pokemon) => <Pokemon key={pokemon.id} pokemon={pokemon}/>)
-
-    return <div>
-      <h1>Pokedex</h1>
+      <div>
+        <h1>Pokedex</h1>
+          <button onClick={this.nextPoke}>Proximo Pokemon</button>
         <div className="pokedex">
-          {pokemonHTMLElements}
         </div>
-      </div>;
+        <Pokemon key={pokemons[count].id} pokemon={pokemons[count]} />
+      </div>
+    );
   };
 }
 
