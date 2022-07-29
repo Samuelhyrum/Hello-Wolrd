a9518bbe376e2646d0c1110a90e607b98632a60b
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-29 11:35:09 -0300

    exercicios de fixação

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/personal-profile/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import PersonDetails from './PersonDetails';
+
+function App() {
+  return (
+    <div className="App">
+    <PersonDetails/>
+    </div>
+  );
+}
+
+export default App;
