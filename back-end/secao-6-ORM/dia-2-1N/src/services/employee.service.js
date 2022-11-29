59ca0bb88207c10cabd7a15e70be3dccc5da9c6c
Samuel Hyrum <samuelhymp@gmail.com>
2022-11-29 18:25:11 -0300

    transactions

--- back-end/secao-6-ORM/dia-2-1N/src/services/employee.service.js
+++ back-end/secao-6-ORM/dia-2-1N/src/services/employee.service.js
@@ -1,6 +1,11 @@
 // src/services/employee.service.js
 
 const { Address, Employee } = require('../models/');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
+
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -27,4 +32,30 @@ const getById = async (id) => {
   return employee;
 }
 
-module.exports = { getAll, getById };
+
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const t = await sequelize.transaction();
+  try {
+    // Depois executamos as operações
+    const result = await sequelize.transaction(async (t) => {
+      const employee = await Employee.create({
+        firstName, lastName, age
+      }, { transaction: t });
+
+      await Address.create({
+        city, street, number, employeeId: employee.id
+      }, { transaction: t });
+      return employee;
+    });
+    return result;
+    // Se chegou até aqui é porque as operações foram concluídas com sucesso,
+    // não sendo necessário finalizar a transação manualmente.
+    // `result` terá o resultado da transação, no caso um empregado e o endereço cadastrado
+  } catch (e) {
+    // Se entrou nesse bloco é porque alguma operação falhou.
+    // Nesse caso, o sequelize irá reverter as operações anteriores com a função rollback, não sendo necessário fazer manualmente
+    console.log(e);
+    throw e; // Jogamos o erro para a controller tratar
+  }
+};
+module.exports = { getAll, getById, insert };
