89ebd2fef89710777f1275ea1204af110775fed1
Samuel Hyrum <samuelhymp@gmail.com>
2023-03-22 10:56:30 -0300

    Exercicios de fixação soma

---
+++ CS/introdução-Python/dia-1-Introdução/operatorSimple.py
@@ -0,0 +1,25 @@
+a = 10
+b = 5
+a + b
+a - b
+a * b
+a / b
+a // b
+a ** b
+a % b
+
+# Exercicio 2
+
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+minutes
+seconds
+
+# Exercicio 4
+
+books = 60
+book_price = (1 - 0.4) * 24.20
+logistic = 3 + (books - 1) * 0.75
+cost = books * book_price + logistic
+cost
