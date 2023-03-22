208fe85fa47caaf4ad68a44d6993a36ce998275a
Samuel Hyrum <samuelhymp@gmail.com>
2023-03-22 11:19:45 -0300

    Listas em python

---
+++ CS/introdução-Python/dia-1-Introdução/lists.py
@@ -0,0 +1,23 @@
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+# Exercício 5
+
+trybe_course.append("Ciência da Computação")
+
+
+# Exercício 6
+
+trybe_course[0] = "Fundamentos"
+
+info = {
+  "personagem": "Margarida",
+  "origem": "Pato Donald",
+  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+}
+
+# Exercício 8
+info["recorrente"] = "Sim"
+
+# Exercício 9
+
+del info["origem"]
