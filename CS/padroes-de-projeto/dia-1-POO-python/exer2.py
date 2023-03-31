1ce7fb07042586e9bf4ce39ae17a97ec877a923a
Samuel Hyrum <samuelhymp@gmail.com>
2023-03-31 19:13:58 -0300

    Exercicio 2, refatorado

--- CS/padroes-de-projeto/dia-1-POO-python/exer2.py
+++ CS/padroes-de-projeto/dia-1-POO-python/exer2.py
@@ -6,21 +6,30 @@ import statistics
 class Estatistica:
     @classmethod
     def media(cls, numbers):
-        return sum(numbers) / len(numbers)
+        # return sum(numbers) / len(numbers)
+        return statistics.mean(numbers)
 
     @classmethod
     def mediana(cls, numbers):
-        numbers.sort()
-        index = len(numbers) // 2
-        if len(numbers) % 2 == 0:
-            return (numbers[index - 1] + numbers[index]) / 2
-        return numbers[index]
+        # numbers.sort()
+        # index = len(numbers) // 2
+        # if len(numbers) % 2 == 0:
+        #     return (numbers[index - 1] + numbers[index]) / 2
+        # return numbers[index]
+        return statistics.median(numbers)
 
 
     @classmethod
     def moda(cls, numbers):
-        number, _ = Counter(numbers).most_common()[0]
-        return number
+        # number, _ = Counter(numbers).most_common()[0]
+        # return number
+        return statistics.mode(numbers)
 
+media = Estatistica.media([1, 2, 5, 3, 8])
 mediana = Estatistica.mediana([1, 2, 5, 3, 8])
+moda = Estatistica.moda([1, 2, 5, 3, 8])
+
+
+print(media)
 print(mediana)
+print(moda)
