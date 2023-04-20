ab0b56c9997fa1ead26009d7ddfe81373335338b
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-20 19:26:17 -0300

    Exercicio 3, cubica

--- CS/algoritimos/dia-1-Complexidade/quadra.py
+++ CS/algoritimos/dia-1-Complexidade/quadra.py
@@ -1,19 +1,17 @@
-def multiply_arrays(array1, array2):
+def multiply_arrays(array1, array2, array3):
     result = []
     number_of_iterations = 0
 
     for number1 in array1:
-        print(f'Array 1: {number1}')
         for number2 in array2:
-            print(f'Array 2: {number2}')
-            result.append(number1 * number2)
-            number_of_iterations += 1
+            for number3 in array3:
+                result.append(number1 * number2 * number3)
+                number_of_iterations += 1
 
     print(f'{number_of_iterations} iterações!')
     return result
 
 
-n = 1000 # onde n será o número total de elementos dentro do array
-meu_array = list(range(1, n))
-
-multiply_arrays(meu_array, meu_array)
+# Usar arrays de tamanho 1000 aqui pode ser muito lento!
+meu_array = list(range(1, 100))
+multiply_arrays(meu_array, meu_array, meu_array)
