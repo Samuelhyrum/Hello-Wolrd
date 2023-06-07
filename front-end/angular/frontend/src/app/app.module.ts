ca66209be668bf08368f8d92f3ecd08d6f4429ef
Samuel Hyrum <samuelhymp@gmail.com>
2023-06-06 23:05:17 -0300

    Navegar para tela de criar funcionario

--- front-end/angular/frontend/src/app/app.module.ts
+++ front-end/angular/frontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -35,7 +36,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatToolbarModule,
     MatListModule,
     MatSidenavModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
