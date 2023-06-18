1270305ff258d093a8f042cfba92901cf3c0bc2f
Samuel Hyrum <samuelhymp@gmail.com>
2023-06-17 22:57:43 -0300

    Resolvendo erros

--- CS/raspagem-de-dados/flask/aula/app/__init__.py
+++ CS/raspagem-de-dados/flask/aula/app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
 from flask_login import LoginManager
+from config import Config
+from app.models.tables import User
+from app.models import db
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+db.init_app(app)
 migrate = Migrate(app, db)
 
-lm = LoginManager(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app.models import tables, forms
 from app.controllers import default
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
