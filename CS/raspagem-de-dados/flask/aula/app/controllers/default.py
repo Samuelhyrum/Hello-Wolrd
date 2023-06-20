4dee940ea238ae77d7ccae580eadda78d1884956
Samuel Hyrum <samuelhymp@gmail.com>
2023-06-19 22:47:54 -0300

    Flask login finalizado

--- CS/raspagem-de-dados/flask/aula/app/controllers/default.py
+++ CS/raspagem-de-dados/flask/aula/app/controllers/default.py
@@ -1,24 +1,35 @@
-from app import app
-from flask import render_template, flash
+from app import app, login_manager
+from flask import render_template, flash, redirect, url_for
 from app.models.forms import LoginForm
 from app.models.tables import User
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 @app.route("/index")
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first() 
-        if user and user.password == form.data.password:
+        if user and user.password == form.password.data:
             login_user(user)  
             flash("Logged in.") 
+            return redirect(url_for("index"))
         else:
             flash("Invalid login.")
-    else:
-        print(form.errors)
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Logged out")
+    return redirect(url_for('index'))
