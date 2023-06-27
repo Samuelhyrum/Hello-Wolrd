40cbb15cdcabe6c2022c82319fdc66ba523f772f
Samuel Hyrum <samuelhymp@gmail.com>
2023-06-26 22:03:29 -0300

    Roteando o botão de atualização

--- front-end/angular/frontend/src/app/app.module.ts
+++ front-end/angular/frontend/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -45,6 +46,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
