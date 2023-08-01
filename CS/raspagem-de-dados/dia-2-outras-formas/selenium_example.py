35cc3fd6892a115b3c1303d3b13e246aa5bbb1c4
Samuel Hyrum <samuelhymp@gmail.com>
2023-07-31 23:54:38 -0300

    Acessando o mercado livre e procurando produto especifico

--- CS/raspagem-de-dados/dia-2-outras-formas/selenium_example.py
+++ CS/raspagem-de-dados/dia-2-outras-formas/selenium_example.py
@@ -2,6 +2,7 @@
 # os principais navegadores da web
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Espera 3 segundos pra iniciar
 sleep(3)
@@ -17,6 +18,12 @@ chrome = webdriver.Chrome(options=options)
 # requisições para essa instância criada utilizando o método `get`
 response = chrome.get("https://www.mercadolivre.com.br/")
 
+search_input = chrome.find_element("name", "as_word")
+search_input.send_keys("BARRA DIRECAO  VW-UP 2014 ED-- ( DIR. ELÉTRICA )")
+
+sleep(3)
+
+search_input.send_keys(Keys.ENTER)
 
 sleep(5)
 # Após 5 segundos vai fechar
