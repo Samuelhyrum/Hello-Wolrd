d539f154d606167a3c3bd197e51ff8fa3baf0a49
Samuel Hyrum <samuelhymp@gmail.com>
2023-08-15 23:06:24 -0300

    Finalização Crawler mercado livre

--- CS/raspagem-de-dados/dia-2-outras-formas/ml/selenium_example.py
+++ CS/raspagem-de-dados/dia-2-outras-formas/ml/selenium_example.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 
+# Declarar a variável global ids_com_erro
+global ids_com_erro
+ids_com_erro = []
+
 # Função para pesquisar e obter links e imagens
 def pesquisar_e_obter_links_e_imagens(item_pesquisado, id):
     # Configuração do navegador
@@ -15,72 +19,80 @@ def pesquisar_e_obter_links_e_imagens(item_pesquisado, id):
     options.add_argument('--ignore-certificate-error')
     options.add_argument('--ignore-ssl-error=yes')
     options.add_argument('--start-maximized')
+    options.add_argument('--headless')  # Adiciona esta linha para executar em modo headless
 
     # Criação de uma instância de navegador utilizando o Chrome
     chrome = webdriver.Chrome(options=options)
 
     # Requisição para a página do Mercado Livre com o item pesquisado
     chrome.get("https://www.mercadolivre.com.br/")
-    search_input = chrome.find_element(By.NAME, "as_word")
-    search_input.send_keys(item_pesquisado)
-    search_input.send_keys(Keys.ENTER)
-    sleep(3)
+    
+
+    try:
+        
+        search_input = chrome.find_element(By.NAME, "as_word")
+        search_input.send_keys(item_pesquisado)
+        search_input.send_keys(Keys.ENTER)
+        sleep(3)    
 
     # Encontra todas as tags <div> que correspondem à classe "ui-search-result__content-wrapper"
-    div_elements = chrome.find_elements(By.CLASS_NAME, "ui-search-result__content-wrapper")
+        div_elements = chrome.find_elements(By.CLASS_NAME, "ui-search-result__content-wrapper")
 
     # Verifica se existem pelo menos 3 elementos <div> na página
-    if len(div_elements) >= 3:
-        # Pega o terceiro elemento <div> (índice 2, pois a contagem começa em zero)
-        third_div = div_elements[2]
+        if len(div_elements) >= 3:
+                # Pega o terceiro elemento <div> (índice 2, pois a contagem começa em zero)
+              third_div = div_elements[2]
 
-        try:
-            link_element = third_div.find_element(By.TAG_NAME, "a")
-            actions = ActionChains(chrome)
-            actions.move_to_element(link_element).perform()
+      
+        link_element = third_div.find_element(By.TAG_NAME, "a")
+        actions = ActionChains(chrome)
+        actions.move_to_element(link_element).perform()
 
-            chrome.execute_script("arguments[0].click();", link_element)
+        chrome.execute_script("arguments[0].click();", link_element)
 
-            wait = WebDriverWait(chrome, 10)
+        wait = WebDriverWait(chrome, 10)
 
-            img_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//img[contains(@class, 'ui-pdp-image ui-pdp-gallery__figure__image')]")))
-            img_src = img_element.get_attribute("src")
+        img_element = wait.until(EC.visibility_of_element_located((By.XPATH, "//img[contains(@class, 'ui-pdp-image ui-pdp-gallery_figure_image')]")))
+        img_src = img_element.get_attribute("src")
 
             # Fecha o navegador
-            chrome.quit()
+        chrome.quit()
 
             # Monta a URL da foto a partir do link final do src da imagem
-            nmFoto = img_src.split("/")[-1]
+        nmFoto = img_src.split("/")[-1]
 
-            idSrv = str(id)
-            
-            insert_imagem_api( img_src, idSrv, "horizontal", nmFoto)
+            # converter id em string
+        string_id = str(id)
 
-        except:
-            print(f"Não foi possível encontrar o link ou obter o atributo src para o item '{item_pesquisado}'.")
-            chrome.quit()
+            # Insere a imagem usando o endpoint fornecido
+        insert_imagem_api(img_src, string_id, "horizontal", nmFoto)
+
+    except Exception as e:
+            print(f"Erro ao processar o item '{item_pesquisado}': {e}")
+            ids_com_erro.append(id)  # Adicione o ID ao array de IDs com erro
+            chrome.quit()  # Certifique-se de fechar o navegador após um erro
 
     else:
         print("Não existem pelo menos 3 elementos <div> na página.")
-
+        
+#IDs com erro: [3860, 4140, 4490, 4588, 4666, 4669, 4673, 4708, 4716, 4741, 4844, 4845, 4846, 4850, 4851, 4852, 4854, 4855, 5005, 5010]
 
 # Função para fazer o insert da imagem na API usando o endpoint fornecido
 def insert_imagem_api(url, idSrv, posicao, nome):
-    url_insert_api = f"https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/InsertFoto/h_srv_prontocarcascavel"
-
-    body = {
+   url_insert_api = f"https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/InsertFoto/h_srv_prontocarcascavel"
+   body = {
         "nome": nome,
         "idSrv": idSrv,
         "posicao": posicao,
         "url": url
     }
-    response_insert = requests.post(url_insert_api, json=body)
- 
+   print(body)
+   response_insert = requests.post(url_insert_api, json=body) 
 
-    if response_insert.status_code == 200:
-        print(f"Inserção da imagem para '{idSrv}' realizada com sucesso!")
-    else:
-        print(f"Falha ao inserir a imagem para '{idSrv}'. Status code: {response_insert.status_code}")
+   if response_insert.status_code == 200:
+    print(f"Inserção da imagem para '{idSrv}' realizada com sucesso!")
+   else:
+    print(f"Falha ao inserir a imagem para '{idSrv}'. Status code: {response_insert.status_code}")
 
 
 url_api = "https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/ProdutosSemImagem/h_srv_prontocarcascavel"
@@ -102,6 +114,7 @@ if response.status_code == 200:
 
             # Chama a função para pesquisar e obter links e imagens do item atual
             pesquisar_e_obter_links_e_imagens(descricao, id_item)
+        print("IDs com erro:", ids_com_erro)  # Exibe os IDs com erro após o loop
 
     else:
         print("A chave 'descrição' não está presente nos dados da API.")
