98058e4386ef0953910167c5a9fee3d6feae375e
Samuel Hyrum <samuelhymp@gmail.com>
2023-08-25 22:18:50 -0300

    Codigo quase pronto, faltando apenas ajeitar a função de segundo plano

--- CS/raspagem-de-dados/dia-2-outras-formas/pharma/pharma_crawler.py
+++ CS/raspagem-de-dados/dia-2-outras-formas/pharma/pharma_crawler.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,11 +11,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # Declarar a variável global ids_com_erro
 global cont
-cont = 0
 global ids_com_erro
-ids_com_erro = []
 global prescricao
-prescricao = 'Sem prescrição'
+ids_com_erro = []
+cont = 0
+prescricao_padrao = 'Sem prescrição'
 
 
 def pesquisar_remedios_ean(ean_remedio):
@@ -24,16 +24,13 @@ def pesquisar_remedios_ean(ean_remedio):
     options.add_argument('--ignore-certificate-error')
     options.add_argument('--ignore-ssl-error=yes')
     options.add_argument('--start-maximized')
-    # options.add_argument('--headless')  # Adiciona esta linha para executar em modo headless
+    options.add_argument('--headless')  # Adiciona esta linha para executar em modo headless
 
     # Criação de uma instância de navegador utilizando o Chrome
     chrome = webdriver.Chrome(options=options)
 
     # Requisição para a página do Mercado Livre com o item pesquisado
     chrome.get("https://consultaremedios.com.br/")
-
-    sleep(3)
-    # Fecha o navegador
 
     try:
 
@@ -45,19 +42,31 @@ def pesquisar_remedios_ean(ean_remedio):
 
         # Encontrar o H1 e pegar o texto correspondente
         h1_text = chrome.find_element(By.TAG_NAME, 'h1').text
-
-        if h1_text == ean_remedio:
-            print("Texto do elemento h1 corresponde ao código EAN")
-            ids_com_erro.append(ean_remedio)  # Adicione o ID ao array de IDs com erro
+        prescricao = chrome.find_element(By.CLASS_NAME, 'new-product-header__leaflet-resume__text').text
+        
+        try:
+            
+            if h1_text == ean_remedio:
+                print("Texto do elemento h1 corresponde ao código EAN, não prosseguir")
+                chrome.quit()  # Certifique-se de fechar o navegador após um erro
+        except Exception as e:
+            print(f"Erro ao processar o item '{ean_remedio}': {e}")
+            chrome.quit()  # Certifique-se de fechar o navegador após um erro
+        
+        try:
+            
+            if not prescricao:
+                print("Prescrição está presente no site, não prosseguir")
+                chrome.quit()  # Certifique-se de fechar o navegador após um erro
+        except Exception as e:
+            print(f"Erro ao processar o item '{ean_remedio}': {e}")
             chrome.quit()  # Certifique-se de fechar o navegador após um erro
         else:
-            print(h1_text)
-            prescricao = chrome.find_element(By.CLASS_NAME, 'new-product-header__leaflet-resume__text').text
+            prescricao_padrao = prescricao
             div_img = chrome.find_element(By.CLASS_NAME, 'new-product-header__top-side__top-left-side')
             img = div_img.find_element(By.TAG_NAME, 'img').get_attribute('src')
-            print(prescricao)
-            print(img)
-            print("Imagem foi encontrada e cadastrada")
+            insert_imagem_api(h1_text,prescricao_padrao,img)
+            #print("Imagem foi encontrada e cadastrada")
 
     except Exception as e:
         print(f"Erro ao processar o item '{ean_remedio}': {e}")
@@ -66,15 +75,16 @@ def pesquisar_remedios_ean(ean_remedio):
 
 
 
-# # Função para fazer o insert da imagem na API usando o endpoint fornecido
-# def insert_imagem_api(url, idSrv, posicao, nome):
+ # Função para fazer o insert da imagem na API usando o endpoint fornecido
+def insert_imagem_api(nome, prescricao, img):
 #    url_insert_api = f"https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/InsertFoto/h_srv_prontocarcascavel"
-#    body = {
-#         "nome": nome,
-#         "idSrv": idSrv,
-#         "posicao": posicao,
-#         "url": url
-#     }
+    body = {
+         "nome": nome,
+         "prescricao": prescricao,
+         "img": img,
+     }
+    
+    print(body)
 
 #    response_insert = requests.post(url_insert_api, json=body) 
 
@@ -84,25 +94,22 @@ def pesquisar_remedios_ean(ean_remedio):
 #     print(f"Falha ao inserir a imagem para '{idSrv}'. Status code: {response_insert.status_code}")
 
 
-# url_api = "https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/RetornaEan"
+url_api = "https://ellenapi.azurewebsites.net//RotinasAjustesImplantacao/RetornaEan"
 
-url_api = [
-'7898040329679',
-    '7891106911733'
-]
 
 # # Requisição GET para a API para obter os produtos sem imagem
-# # response = requests.get(url_api)
+#url_api = [ '7896658033414','7891106911733','7898040329679']
+response = requests.get(url_api)
 
 # # Verifica se a resposta da API foi bem-sucedida
-# # if response.status_code == 200:
+if response.status_code == 200:
 #     # Converte o conteúdo JSON da resposta para um dicionário Python
-#     # data = response.json()
+   data = response.json()
 #     # Percorre os itens pesquisados
-for item in url_api:
+for item in data:
         # Chama a função para pesquisar 
-        pesquisar_remedios_ean(item)
-        cont +=1
+    print(item)
+    cont +=1
 print("IDs com erro:", ids_com_erro, cont)  # Exibe os IDs com erro após o loop
 
 # else:
