955eb7325e3ac999ae58d18df02cb8c2f2332261
Samuel Hyrum <samuelhymp@gmail.com>
2023-09-21 20:14:58 -0300

    Connection e criando table

--- CS/raspagem-de-dados/dia-2-outras-formas/pharma/conection.php
+++ CS/raspagem-de-dados/dia-2-outras-formas/pharma/conection.php
@@ -1,6 +1,5 @@
 <?php
-
-//Constantes para armazenamento das variáveis de conexão.
+// Constantes para armazenamento das variáveis de conexão.
 define('HOST', '127.0.0.1');
 define('PORT', '3306');
 define('DBNAME', 'php_db');
@@ -8,9 +7,10 @@ define('USER', 'root');
 define('PASSWORD', 'password');
 
 try {
-	$dsn = new PDO("mysql:host=". HOST . ";port=".PORT.";dbname=" . DBNAME .";user=" . USER . ";password=" . PASSWORD);
-    echo 'Login feito';
+    $dsn = new PDO("mysql:host=". HOST . ";port=".PORT.";dbname=" . DBNAME, USER, PASSWORD);
+    $dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexão estabelecida com sucesso';
 } catch (PDOException $e) {
-	echo 'A conexão falhou e retornou a seguinte mensagem de erro: ' .$e->getMessage();
+    echo 'A conexão falhou e retornou a seguinte mensagem de erro: ' .$e->getMessage();
 }
-$dsn = null;
+?>
