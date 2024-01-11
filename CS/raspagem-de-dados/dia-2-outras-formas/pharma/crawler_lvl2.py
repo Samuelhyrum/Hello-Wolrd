d4426f6ac826a30e8a0a73cfe15e311451f309db
Samuel Hyrum <samuelhymp@gmail.com>
2024-01-10 22:40:40 -0300

    Script pronto

--- CS/raspagem-de-dados/dia-2-outras-formas/pharma/crawler_lvl2.py
+++ CS/raspagem-de-dados/dia-2-outras-formas/pharma/crawler_lvl2.py
@@ -25,7 +25,7 @@ def pesquisar_remedios_nome(name_remedio, product_gtin):
 
 
 
-    # Criação de uma instância de navegador utilizando
+
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
     options = webdriver.ChromeOptions()
     service = ChromeService(executable_path=ChromeDriverManager().install())
